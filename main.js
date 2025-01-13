ee9cccfc5a89944ea5276db612b486fd6b7ed9bc
Claire <cmn17@uw.edu>
2025-01-12 20:02:11 -0800

    NARUTO RUNNNNNNN

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./naruto.png")
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new Naruto(gameEngine));
 
 	gameEngine.init(ctx);
 
